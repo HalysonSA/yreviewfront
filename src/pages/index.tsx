4224fc1a91c4a1df794f1ddf8138f2b1c55899bd
Halyson <92060880+HalysonSA@users.noreply.github.com>
2023-04-16 00:46:54 -0300

    sending user review data to server

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,124 +1,115 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useForm } from "react-hook-form";
+import { BsFillEmojiFrownFill, BsFillEmojiSmileFill } from "react-icons/bs";
+import axios from "./api/axios";
 
-const inter = Inter({ subsets: ['latin'] })
+type TFormData = {
+  title: string;
+  description: string;
+  rating: number;
+  image: string[];
+};
 
 export default function Home() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm<TFormData>();
+
+  const onSubmit = handleSubmit(async (data) => {
+    const formData = new FormData();
+
+    const review = await axios.post("/review", {
+      title: data.title,
+      description: data.description,
+      rating: Number(data.rating),
+    });
+
+    const images = Array.from(data.image);
+
+    const a = images.map(async (image) => {
+      formData.set("image", image);
+      const response = await axios.post(
+        `/image/${review.data.data.id}`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+          onUploadProgress: (progressEvent) => {
+            const { loaded, total } = progressEvent;
+            //@ts-ignore
+            let progress: number = Math.round((loaded * 100) / total); // total is possibly undefined
+            console.log(`A imagem está ${progress}% carregada... `);
+          },
+        }
+      );
+      return response.data;
+    });
+
+    console.log(await Promise.all(a));
+    reset();
+  });
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className='w-screen min-h-screen flex flex-col justify-center items-center bg-[#465076] '>
+      <div className='min-w-[600px] p-4 bg-white rounded-[10px] min-h-[700px] '>
+        <form
+          id='review-form'
+          onSubmit={onSubmit}
+          className='flex flex-col gap-4'>
+          <div className='flex flex-col gap-1'>
+            <label htmlFor=''>Titulo</label>
+            <input
+              type='text'
+              {...register("title")}
+              className='border border-black h-10 rounded-[10px] p-2'
             />
-          </a>
-        </div>
+          </div>
+          <div className='flex flex-col gap-1 '>
+            <label htmlFor=''>Descrição</label>
+            <textarea
+              {...register("description")}
+              className='border border-black rounded-[10px] min-h-[80px] p-2'
+            />
+          </div>
+          <div className='flex flex-col gap-1 max-w-[50%]'>
+            <label htmlFor=''>Avaliação</label>
+
+            <input
+              type='range'
+              min={0}
+              max={5}
+              {...register("rating")}
+              className='text-orange-400'
+            />
+            <div className='flex flex-row items-center justify-between my-1'>
+              <BsFillEmojiFrownFill fill='#040552' size={40} />
+              <BsFillEmojiSmileFill fill='#e0bf00' size={40} />
+            </div>
+          </div>
+          <label htmlFor='select-file' tabIndex={0} className='file'>
+            Anexe suas imagens
+          </label>
+          <input
+            id='select-file'
+            multiple
+            type='file'
+            accept='image/*'
+            {...register("image")}
+          />
+          <div className='flex justify-end w-full'>
+            <button
+              type='submit'
+              className='uppercase bg-green-700 text-white rounded-[10px] px-10 py-4'>
+              salvar
+            </button>
+          </div>
+        </form>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <div></div>
+    </div>
+  );
 }
